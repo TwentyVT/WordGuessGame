d9e84b1b9a067e74aa43c18bb1d4f73cbdd90733
TwentyVT <sonatore@amazon.com>
2018-09-30 15:34:44 -0400

    letter/dashes set, word bank made

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,42 @@
 
+// setting word bank array
+// picking random word to guess from wordbank array
 
-var userGuessText = document.getElementById("user-guess");
+var word_bank = ["one", "two", "three", "beef", "can", "man"];
+var word_to_guess = word_bank[Math.floor(Math.random() * word_bank.length)]
+console.log(word_to_guess);
+var good_letters = [];
+var dom_word = document.getElementById("word");
+var tries = 15;
+var triesleft = document.getElementById("triesleft");
 
-var word = ["a", "b", "c"];
-
-var dog = 5; 
-
-
-document.onkeyup = function (guess) {
-
-    var userGuessText = document.getElementById("user-guess");
-
-    var word = ["b", "o", "b"];
-
-    var userGuess = guess.key.toLowerCase();
-    console.log(userGuess);
-
-     
-    for (i = 0; i < word.length -1; i++ ) {
-    
-        
-    if ((word.indexOf(userGuess) > -1)) {
-        userGuessText.innerHTML += userGuess;
-    } else {
-        console.log("NO WORK")
+function print() {
+    var print_word = "";
+    document.getElementById("word").innerHTML = print_word;
+       for (var i = 0; i < word_to_guess.length; i++) {
+        if (good_letters.indexOf(word_to_guess[i]) > -1) {
+            print_word = print_word + " " + word_to_guess[i] + " ";
+        } else {
+            print_word += " _ ";
+        }
     }
+    dom_word.innerHTML = print_word;
+
 }
+document.onkeyup = function (event) {
+    if (word_to_guess.indexOf(event.key) > -1) {
+        good_letters.push(event.key);
+    } else {
+        console.log("HELLO")
+        // var tries = tries--;
+        // triesleft.innerHTML = tries;
+    }
+    print();
 }
+
+print();
+
+
 
 
 
