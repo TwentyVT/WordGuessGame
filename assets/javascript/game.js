c1e7d7bea8b94101e97787a6386eed8cb41c94be
TwentyVT <sonatore@amazon.com>
2018-09-30 19:38:26 -0400

    good letters and bad letters are fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,11 +6,13 @@ var word_bank = ["one", "two", "throw", "bet", "can", "man"];
 var word_to_guess = word_bank[Math.floor(Math.random() * word_bank.length)]
 console.log(word_to_guess);
 var good_letters = [];
+var guessedletters = [];
 var dom_word = document.getElementById("word");
 var triesleft = document.getElementById("triesleft");
 var badletters = document.getElementById("badletters");
 var tries = 15;
 var wintrigger = 0;
+var winbanner = "";
 
 function triesCounter() {
     if (word_to_guess.indexOf(event.key) <= -1) {
@@ -18,11 +20,16 @@ function triesCounter() {
         tries = parseInt(tries - 1);
         triesleft.textContent = tries;
         badletters.innerHTML += " " + event.key;
-        }
-        if (tries === 0) {
-            badletters.innerHTML = "YOU LOSE GOOD DAY SIR";
-            reset();
     }
+    if (tries === 0) {
+        badletters.innerHTML = "YOU LOSE GOOD DAY SIR";
+        reset();
+    }
+}
+
+function alreadyGuessed() {
+    if (guessedletters.indexOf(event.key) > -1)
+        alert("you guessed!")
 }
 
 function reset() {
@@ -51,20 +58,25 @@ function print() {
         dom_word.innerHTML = print_word;
         if (good_letters.length === word_to_guess.length) {
             dom_word.innerHTML = "You win";
-            reset();    
+            reset();
         }
-        }
-
     }
 
-        document.onkeyup = function (event) {
+}
 
-            if (word_to_guess.indexOf(event.key) > -1) {
-                good_letters.push(event.key);
-            } else {
-                triesCounter();
-            }
-            print();
-        }
-        print();
+document.onkeyup = function (event) {
+
+    if (good_letters.indexOf(event.key) > -1) {
+        alert("u already guessed that!")
+    } else if (word_to_guess.indexOf(event.key) > -1) {
+        good_letters.push(event.key);
+    } else if (guessedletters.indexOf(event.key) > -1) {
+        alert("u dun goofed")
+    } else {
+        triesCounter();
+        guessedletters.push(event.key);
+    }
+    print();
+}
+print();
 
